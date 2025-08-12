e03312fe159682fe6b0e9136b00ccbe7e67eed1c
Madhuri Panchware <134092881+MADDY711@users.noreply.github.com>
2025-08-12 19:46:09 +0530

    Update run_pipeline.py

--- ocr_doc_verifier/run_pipeline.py
+++ ocr_doc_verifier/run_pipeline.py
@@ -1,32 +1,23 @@
 import cv2
-
 from resize_img import resize_image
 from grayscal_img import convert_to_grayscale
 from preprocessor import preprocess_image
 from extract_text import extract_text_from_image
 from load_img import load_and_fix_orientation
 
-
-
-if __name__ == "__main__":
-  
-    image_path = r"F:\clg\internships\clg_internship\auto_vkyc\OCR_doc_verifier\Untitled.jpg"
+def get_extracted_text(image_path):
     original_image = cv2.imread(image_path)
-
-  
     resized = resize_image(original_image)
-
-
     image = load_and_fix_orientation(image_path)
     resized = resize_image(original_image)
-
     grayscale = convert_to_grayscale(resized)
-
     final_image = preprocess_image(grayscale)
-
     cv2.imwrite("final_preprocessed.png", final_image)
-
     text = extract_text_from_image(final_image)
+    return text
 
+if __name__ == "__main__":
+    image_path = r"D:\internship\clg_internship\automated-video-KYC\ocr_doc_verifier\test_imgs\hd_1.jpeg"
+    extracted_text = get_extracted_text(image_path)
     print("Extracted Text:\n")
-    print(text)
+    print(extracted_text)
